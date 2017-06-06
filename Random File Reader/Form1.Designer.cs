37df85d7683c3ea8ca1b3e6d4cf69e0e9d0a7570
spenc <spenc@LAPTOP-8LL6OQLE>
2017-06-06 13:37:56 -0600

    Add project files.

---
+++ Random File Reader/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Random_File_Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.qtyNumbersLabel = new System.Windows.Forms.Label();
+            this.qtyDisplayLabel = new System.Windows.Forms.Label();
+            this.sumDisplayLabel = new System.Windows.Forms.Label();
+            this.randNumListBox = new System.Windows.Forms.ListBox();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(38, 18);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(203, 24);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Random File Reader";
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(108, 291);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(75, 23);
+            this.openFileButton.TabIndex = 1;
+            this.openFileButton.Text = "Open File";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(29, 378);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(109, 13);
+            this.totalLabel.TabIndex = 2;
+            this.totalLabel.Text = "Total of the Numbers:";
+            // 
+            // qtyNumbersLabel
+            // 
+            this.qtyNumbersLabel.AutoSize = true;
+            this.qtyNumbersLabel.Location = new System.Drawing.Point(29, 335);
+            this.qtyNumbersLabel.Name = "qtyNumbersLabel";
+            this.qtyNumbersLabel.Size = new System.Drawing.Size(147, 13);
+            this.qtyNumbersLabel.TabIndex = 3;
+            this.qtyNumbersLabel.Text = "Number of Random Numbers:";
+            // 
+            // qtyDisplayLabel
+            // 
+            this.qtyDisplayLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.qtyDisplayLabel.Location = new System.Drawing.Point(194, 334);
+            this.qtyDisplayLabel.Name = "qtyDisplayLabel";
+            this.qtyDisplayLabel.Size = new System.Drawing.Size(68, 22);
+            this.qtyDisplayLabel.TabIndex = 4;
+            // 
+            // sumDisplayLabel
+            // 
+            this.sumDisplayLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.sumDisplayLabel.Location = new System.Drawing.Point(194, 369);
+            this.sumDisplayLabel.Name = "sumDisplayLabel";
+            this.sumDisplayLabel.Size = new System.Drawing.Size(68, 22);
+            this.sumDisplayLabel.TabIndex = 5;
+            // 
+            // randNumListBox
+            // 
+            this.randNumListBox.FormattingEnabled = true;
+            this.randNumListBox.Location = new System.Drawing.Point(42, 81);
+            this.randNumListBox.Name = "randNumListBox";
+            this.randNumListBox.Size = new System.Drawing.Size(220, 186);
+            this.randNumListBox.TabIndex = 6;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(108, 417);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 452);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.randNumListBox);
+            this.Controls.Add(this.sumDisplayLabel);
+            this.Controls.Add(this.qtyDisplayLabel);
+            this.Controls.Add(this.qtyNumbersLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Random File Reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label qtyNumbersLabel;
+        private System.Windows.Forms.Label qtyDisplayLabel;
+        private System.Windows.Forms.Label sumDisplayLabel;
+        private System.Windows.Forms.ListBox randNumListBox;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Random File Reader/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Random_File_Reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader inputFile; // Declare StreamReader Object: "inputFile"
+
+                if (openFile.ShowDialog() == DialogResult.OK) // if the user clicks "OK" or "Open" in the open file dialog box, run the code inside this block
+                {
+                    // Declare variables for the sum and quantity of the random numbers, as well as a variable to act as a vehicle to carry the random numbers to the listBox.
+                    int sumOfNumbers = 0;
+                    int qtyOfNumbers = 0;
+                    int randomNumber;
+
+
+                    inputFile = File.OpenText(openFile.FileName); //assign the file being opened to the inputFile object
+
+                    //Clear the ListBox and display labels so that reloading a file will load all new values.
+                    randNumListBox.Items.Clear();
+                    qtyDisplayLabel.Text = "";
+                    sumDisplayLabel.Text = "";
+
+                    // At this point the selected file is assigned to the "inputFile" object and the display fields are clear
+
+                    while (!inputFile.EndOfStream) // this loop reads a line (a random number) from the file, assigns it to the variable "randomNumber" and adds it to the listbox as an item.
+                    {
+                        randomNumber = int.Parse(inputFile.ReadLine());
+                        randNumListBox.Items.Add(randomNumber);
+                        sumOfNumbers = sumOfNumbers + randomNumber;
+                    } // end of loop
+
+                    inputFile.Close(); // close the file
+
+                    // Display the number of random numbers contained in the file:
+                    qtyOfNumbers = randNumListBox.Items.Count;
+                    qtyDisplayLabel.Text = qtyOfNumbers.ToString();
+
+                    // Display the sum of the numbers in the file:
+                    sumDisplayLabel.Text = sumOfNumbers.ToString("#,##0"); // format number with comma separated thousands place
+
+                } // end of code block for if statement validating that "Open" was clicked.
+            } // End of try block
+
+            catch (Exception ex) // if anything fails in the above block, this will display appropriate message boxes. 
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }// End openFileButton_Click block
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
